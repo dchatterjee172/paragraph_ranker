ef7836c4a8aabafd94a89a776aef729a66be6afb
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-03-29 19:19:38 +0530

    update

--- rank.py
+++ rank.py
@@ -4,8 +4,8 @@ import numpy as np
 
 def model_builder(embedding_, context_):
     def model(features, labels, mode, params):
-        embedding = tf.get_variable("embedding", size=embedding_.size, trainable=False)
-        context = tf.get_variable("embedding", size=context_.size, trainable=False)
+        embedding = tf.get_variable("embedding", shape=embedding_.size, trainable=False)
+        context = tf.get_variable("context", shape=context_.size, trainable=False)
 
         def init_fn(scaffold, sess):
             sess.run(embedding.initializer, {embedding.initial_value: embedding_})
@@ -15,7 +15,7 @@ def model_builder(embedding_, context_):
         q = features["q"]
         if mode == tf.estimator.ModeKeys.TRAIN:
             scaffold = tf.train.Scaffold(init_fn=init_fn)
-            loss = 0
+            loss = tf.constant(0.0)
             optimizer = tf.train.AdamOptimizer()
             train = optimizer.minimize(
                 loss, global_step=tf.train.get_or_create_global_step()
@@ -32,34 +32,71 @@ def model_builder(embedding_, context_):
     return model
 
 
-def input_fn(input_file, is_training, batch_size, sample_size, total_context):
-    name_to_features = {
-        "context_id": tf.FixedLenFeature([1], tf.int64),
-        "q": tf.FixedLenFeature([300], tf.int64),
-    }
+def input_fn_builder(input_file, is_training, batch_size, sample_size, total_context):
+    def input_fn():
+        name_to_features = {
+            "context_id": tf.FixedLenFeature([1], tf.int64),
+            "q": tf.FixedLenFeature([300], tf.int64),
+        }
 
-    def _decode_record(record, name_to_features):
-        example = tf.parse_single_example(record, name_to_features)
-        sample = tf.random.uniform(shape=[sample_size - 1], max_val=total_context)
-        example["context_id"] = tf.concat([example["context_id"], sample])
-        return example
+        def _decode_record(record, name_to_features):
+            example = tf.parse_single_example(record, name_to_features)
+            sample = tf.random.uniform(
+                shape=[sample_size - 1], maxval=total_context, dtype=tf.int64
+            )
+            example["context_id"] = tf.concat([example["context_id"], sample], axis=-1)
+            return example
 
-    d = tf.data.TFRecordDataset(input_file)
-    drop_remainder = False
-    if is_training:
-        d = d.repeat()
-        d = d.shuffle(buffer_size=100)
-        drop_remainder = True
-    d = d.apply(
-        tf.contrib.data.map_and_batch(
-            lambda record: _decode_record(record, name_to_features),
-            batch_size=batch_size,
-            drop_remainder=drop_remainder,
+        d = tf.data.TFRecordDataset(input_file)
+        drop_remainder = False
+        if is_training:
+            d = d.repeat()
+            d = d.shuffle(buffer_size=100)
+            drop_remainder = True
+        d = d.apply(
+            tf.data.experimental.map_and_batch(
+                lambda record: _decode_record(record, name_to_features),
+                batch_size=batch_size,
+                drop_remainder=drop_remainder,
+            )
         )
-    )
-    return d
+        return d
+
+    return input_fn
 
 
 if __name__ == "__main__":
     emb = np.load("embedding.npy")
     contexts = np.load("all_context.npy")
+    model_fn = model_builder(emb, contexts)
+    run_config = tf.estimator.RunConfig(
+        model_dir="tmp",
+        save_summary_steps=100,
+        save_checkpoints_steps=1000,
+        keep_checkpoint_max=2,
+        log_step_count_steps=1,
+    )
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=input_fn_builder(
+            input_file="test.tfrecord",
+            is_training=False,
+            batch_size=2,
+            sample_size=5,
+            total_context=len(contexts),
+        ),
+        steps=100,
+        start_delay_secs=0,
+        throttle_secs=120,
+    )
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=input_fn_builder(
+            input_file="test.tfrecord",
+            is_training=False,
+            batch_size=2,
+            sample_size=5,
+            total_context=len(contexts),
+        ),
+        max_steps=200,
+    )
+    estimator = tf.estimator.Estimator(model_fn=model_fn, config=run_config)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
