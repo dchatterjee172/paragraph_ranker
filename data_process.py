fef80adb8b060d2fb7a8aa0b950a58a6006725bc
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-03-29 16:46:19 +0530

    data process

---
+++ data_process.py
@@ -0,0 +1,126 @@
+from squad_df import v1
+import spacy
+from tqdm import tqdm
+import numpy as np
+import tensorflow as tf
+
+
+def get_word2vec(word_set):
+    glove_path = "/home/dj/data/glove/glove.840B.300d.txt"
+    word2vec_dict = {}
+    with open(glove_path, "r", encoding="utf-8") as fh:
+        for line in tqdm(fh, total=int(2.2e6), desc="getting wordvec"):
+            array = line.lstrip().rstrip().split(" ")
+            word = array[0]
+            vector = list(map(float, array[1:]))
+            if word in word_set:
+                word2vec_dict[word] = vector
+            elif word.capitalize() in word_set:
+                word2vec_dict[word.capitalize()] = vector
+            elif word.lower() in word_set:
+                word2vec_dict[word.lower()] = vector
+            elif word.upper() in word_set:
+                word2vec_dict[word.upper()] = vector
+
+    print(
+        "{}/{} of word vocab have corresponding vectors in {}".format(
+            len(word2vec_dict), len(word_set), glove_path
+        )
+    )
+    return word2vec_dict
+
+
+class FeatureWriter(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.num_features = 0
+        self._writer = tf.python_io.TFRecordWriter(filename)
+
+    def process_feature(self, feature):
+        self.num_features += 1
+
+        def create_int_feature(values):
+            feature = tf.train.Feature(
+                int64_list=tf.train.Int64List(value=list(values))
+            )
+            return feature
+
+        features = dict()
+        features["q"] = create_int_feature(feature[0])
+        features["context_id"] = create_int_feature([feature[1]])
+
+        tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+        self._writer.write(tf_example.SerializeToString())
+
+    def close(self):
+        self._writer.close()
+
+
+nlp = spacy.load("en_core_web_sm", disable=["parser", "tagger"])
+all_context_id = dict()
+all_context = list()
+all_words = set()
+data = [[], []]
+for sample in tqdm(v1, total=98169, desc="processing"):
+    context_id = f"{sample['wiki_id']}_{sample['para_id']}"
+    if context_id not in all_context_id:
+        context = [token.text for token in nlp(sample["context"])]
+        if len(context) > 300:
+            continue
+        all_words.update(context)
+        all_context.append(context)
+        all_context_id[context_id] = len(all_context)
+    question = [token.text for token in nlp(sample["question"])]
+    if len(question) > 20:
+        continue
+    all_words.update(question)
+    data[int(sample["is_train"])].append([question, all_context_id[context_id]])
+
+print(len(data[0]), len(data[1]))
+
+np.random.shuffle(data[0])
+np.random.shuffle(data[1])
+
+word_vecs = get_word2vec(all_words)
+del all_words, all_context_id
+embedding_mat = list()
+word_to_index = dict()
+
+for c in tqdm(range(len(all_context)), total=len(all_context)):
+    for t in range(len(all_context[c])):
+        token = all_context[c][t]
+        if token not in word_to_index and token in word_vecs:
+            all_context[c][t] = len(word_to_index) + 1
+            word_to_index[token] = len(word_to_index) + 1
+            embedding_mat.append(word_vecs[token])
+        elif token not in word_vecs:
+            all_context[c][t] = 1
+        else:
+            all_context[c][t] = word_to_index[token]
+    if len(all_context[c]) < 300:
+        all_context[c] += [0] * (300 - len(all_context[c]))
+
+writers = (FeatureWriter("train.tfrecord"), FeatureWriter("test.tfrecord"))
+
+for i in range(2):
+    for q in tqdm(range(len(data[i])), total=len(data[i])):
+        for t in range(len(data[i][q][0])):
+            token = data[i][q][0][t]
+            if token not in word_to_index and token in word_vecs:
+                data[i][q][0][t] = len(word_to_index) + 1
+                word_to_index[token] = len(word_to_index) + 1
+                embedding_mat.append(word_vecs[token])
+            elif token not in word_vecs:
+                data[i][q][0][t] = 1
+            else:
+                data[i][q][0][t] = word_to_index[token]
+        if len(data[i][q][0]) < 20:
+            data[i][q][0] += [0] * (20 - len(data[i][q][0]))
+        writers[i].process_feature(data[i][q])
+
+map(lambda x: x.close(), writers)
+
+del word_vecs
+embedding_mat.insert(0, [0] * 300)
+embedding_mat.insert(1, np.random.uniform(-0.05, 0.05, (100,)))
+np.save("embedding", embedding_mat)
