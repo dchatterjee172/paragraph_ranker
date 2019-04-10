0bc9c3c0d182ba3f8535498055e8a5095598f15c
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-04-10 15:13:36 +0530

    adding pos embedding before conv does not make a lot of sense

--- rank.py
+++ rank.py
@@ -15,17 +15,17 @@ flags.DEFINE_integer("top_k", 10, "checking if correct para is in top k")
 
 
 def model_builder(embedding_, context_, sample_size):
-    num_units = 100
-    num_vector = 10
+    num_units = 128
+    num_vector = 8
     pos_embedding_ = np.ones((300, 300), dtype=np.float32)
-    ls = 301
-    le = 301
+    ls = 151
+    le = num_units // num_vector
     for k in range(1, le):
         for j in range(1, ls):
             pos_embedding_[j - 1, k - 1] = (1.0 - j / ls) - (k / le) * (
                 1.0 - 2.0 * j / ls
             )
-    pos_embedding_ = np.expand_dims(pos_embedding_, 0)
+    pos_embedding_ = np.expand_dims(pos_embedding_, 0) / 2.0
 
     def _extractor(
         _input,
@@ -33,6 +33,7 @@ def model_builder(embedding_, context_, sample_size):
         h_size,
         is_training,
         batch_size,
+        pos,
         k_size=7,
         strides=2,
         sample_size=sample_size,
@@ -47,13 +48,14 @@ def model_builder(embedding_, context_, sample_size):
                 strides=strides,
                 activation=tf.nn.leaky_relu,
             )
+            seq_len = tf.shape(input_)[-2]
+            input_ = input_ + pos[:, :seq_len, :]
             input_ = tf.layers.batch_normalization(input_, training=is_training)
             score = tf.nn.softmax(
                 tf.matmul(input_, input_, transpose_b=True)
                 / tf.sqrt(tf.constant(h_size, dtype=tf.float32))
             )
             p = tf.matmul(score, input_)
-            seq_len = tf.shape(input_)[-2]
             p = tf.layers.dense(
                 tf.reshape(p, [-1, h_size]),
                 1,
@@ -111,16 +113,13 @@ def model_builder(embedding_, context_, sample_size):
         context = tf.nn.embedding_lookup(all_context, context_id)
         context = tf.nn.embedding_lookup(embedding, context)
         with tf.variable_scope("q"):
-            q = q + pos_embedding[:, :20, :]
-            q = tf.contrib.layers.layer_norm(
-                q, begin_norm_axis=-1, begin_params_axis=-1
-            )
             q = _extractor(
                 q,
                 num_vector,
                 num_units // num_vector,
                 is_training,
                 batch_size,
+                pos_embedding,
                 k_size=4,
                 strides=1,
                 sample_size=1,
@@ -129,12 +128,13 @@ def model_builder(embedding_, context_, sample_size):
             context = tf.reshape(
                 context, [batch_size * sample_size, -1, embedding_.shape[-1]]
             )
-            context = context + pos_embedding
-            context = tf.contrib.layers.layer_norm(
-                context, begin_norm_axis=-1, begin_params_axis=-1
-            )
             context = _extractor(
-                context, num_vector, num_units // num_vector, is_training, batch_size
+                context,
+                num_vector,
+                num_units // num_vector,
+                is_training,
+                batch_size,
+                pos_embedding,
             )
             context = tf.reshape(context, [batch_size, sample_size, -1])
         q = tf.expand_dims(q, -2)
